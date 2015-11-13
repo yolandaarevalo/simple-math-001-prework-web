791aea0f6c4819e1c11cd5f00a1ca76521f87116
yolandaarevalo <yoliarevalo53@gmail.com>
2015-11-13 16:19:35 +0000

    FINISHEDgit statusgit status

--- lib/math.rb
+++ lib/math.rb
@@ -1,21 +1,27 @@
-def addition(num1, num2)
+def addition(num1=5, num2=4)
+  num1 + num2
 end
 
-def subtraction(num1, num2)
+def subtraction(num1=10, num2=5)
+  num1 - num2
 end
 
-def division(num1, num2)
+def division(num1=50, num2=2)
+  num1 / num2
 end
 
-def multiplication(num1, num2)
+def multiplication(num1=4, num2=30)
+  num1 * num2
 end
 
-def modulo(num1, num2)
+def modulo(num1=34, num2=4)
+  num1 % num2 
 end
 
-def square_root(num)
+def square_root(num=81)
+  Math.sqrt(num)
 end
 
 def order_of_operation(num1, num2, num3, num4)
-  #Hint:  __ + (( __ * __ ) / __ )
+  num1 + (( num2 * num3 ) / num4)
 end
